3dbef39a9b367ee09f15da1480dce02d98a3982c
carlo029 <antioquia_carlo2006@yahoo.com>
2015-01-23 14:48:14 +0800

    Added comments for documentation

--- src/textfiles/FileData.java
+++ src/textfiles/FileData.java
@@ -1,3 +1,13 @@
+/*
+ * This class opens a text file in a specified directory and then outputs the lines of the text file to the java console
+ * @author Carlo Antioquia
+ * @version 1
+ * Reference: http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html
+ */
+
+
+
+
 package textfiles;
 import java.io.IOException;
 
@@ -5,7 +15,10 @@ import java.io.IOException;
 
 public class FileData {
 	public static void main (String args[]) throws IOException{
-	
+	/*
+	 * This determines the directory of the file and outputs the lines of the text file
+	 * @throws IOException
+	 */
 		
 		String file_name = "C:/Users/Carlo/Desktop/cie123project.txt";
 		
--- src/textfiles/ReadFile.java
+++ src/textfiles/ReadFile.java
@@ -1,3 +1,12 @@
+/*
+ * This class opens a text file, gets the lines one by one and passes it to the FileData class for display
+ * @author Carlo Antioquia
+ * @version 1
+ * Reference: http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html
+ */
+
+
+
 package textfiles;
 
 import java.io.IOException;
@@ -5,13 +14,26 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class ReadFile {
-	
+	/*
+	 * This function gets the file directory from the FileData class and opens the file
+	 */
 	private String path;
+	/*
+	 * @param String file_path
+	 * the variable file_path is the directory of the file to be opened
+	 */
 	public ReadFile(String file_path){
 		path = file_path;
 	}
 	
 	public String[] OpenFile() throws IOException {
+		
+		/*
+		 * this gets each line of the text file one by one and stores it in a buffer for display later
+		 * @throws IOException
+		 * @return textData
+		 * textData contains the lines of the text file
+		 */
 		
 		FileReader fr = new FileReader(path);
 		BufferedReader textReader = new BufferedReader(fr);
@@ -30,7 +52,11 @@ public class ReadFile {
 	}
 	
 	int readLines() throws IOException{
-		
+		/*
+		 * this function gets the number of lines in the text file
+		 * @return numberOfLines
+		 * numberOfLines is the variable that stores the number of lines the text file has
+		 */
 		FileReader file_to_read =  new FileReader(path);
 		BufferedReader bf = new BufferedReader(file_to_read);
 		
