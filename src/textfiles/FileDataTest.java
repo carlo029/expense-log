c5b24923209bcda975ed88a58983ca31431988bb
carlo029 <antioquia_carlo2006@yahoo.com>
2015-01-30 14:02:26 +0800

    Added a function that lets the user specify the text file to be opened

---
+++ src/textfiles/FileDataTest.java
@@ -0,0 +1,14 @@
+package textfiles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileDataTest {
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ src/textfiles/ReadFileTest.java
@@ -0,0 +1,24 @@
+package textfiles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReadFileTest {
+
+	@Test
+	public void testReadFile() {
+		//assertEquals()
+	}
+
+	@Test
+	public void testOpenFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadLines() {
+		fail("Not yet implemented");
+	}
+
+}
