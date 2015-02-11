c46dd89d36f7683e374cfe1cb9632ccc087539c6
carlo029 <antioquia_carlo2006@yahoo.com>
2015-02-11 16:02:10 +0800

    Added stress test. The test writes 1000 lines to the textfile inside a specified directory (in my computer) as well as the time and date that the stress test was run. The string length per line is 52 characters

--- src/textfiles/writetofileTest.java
+++ src/textfiles/writetofileTest.java
@@ -15,6 +15,9 @@ import org.junit.Test;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class writetofileTest {
 
@@ -24,8 +27,11 @@ public class writetofileTest {
 	
 	@Test
 	public void testWritetofileString() {
-		path= "C:/Users/Dell/Documents/CIE 123/Project/data.txt"; 
-		assertTrue( writetofile("C:/Users/Dell/Documents/CIE 123/Project/data.txt"));
+		/*path= "C:/Users/Dell/Documents/CIE 123/Project/data.txt"; 
+		assertTrue( writetofile("C:/Users/Dell/Documents/CIE 123/Project/data.txt"));*/
+		path= "D:/My Files/5th year 2nd sem/CIE123/project/data.txt"; 
+		assertTrue( writetofile("D:/My Files/5th year 2nd sem/CIE123/project/data.txt"));
+		
 	}
 
 	private void assertTrue(Object writetofile) {
@@ -33,10 +39,14 @@ public class writetofileTest {
 
 	@Test
 	public void testWritetofileStringBoolean() {
-		path= "C:/Users/Dell/Documents/CIE 123/Project/data.txt"; 
+		/*path= "C:/Users/Dell/Documents/CIE 123/Project/data.txt"; 
 		assertTrue( writetofile("C:/Users/Dell/Documents/CIE 123/Project/data.txt"));
 		append_to_file= true; 
-		assertTrue( writetofile("C:/Users/Dell/Documents/CIE 123/Project/data.txt"));
+		assertTrue( writetofile("C:/Users/Dell/Documents/CIE 123/Project/data.txt"));*/
+		path= "D:/My Files/5th year 2nd sem/CIE123/project/data.txt"; 
+		assertTrue( writetofile("D:/My Files/5th year 2nd sem/CIE123/project/data.txt"));
+		append_to_file= true; 
+		assertTrue( writetofile("D:/My Files/5th year 2nd sem/CIE123/project/data.txt"));
 	}
 
 	private Object writetofile(String string) {
@@ -46,9 +56,23 @@ public class writetofileTest {
 
 	@Test
 	public void testWriteToFile() throws IOException {
-		FileWriter write = new FileWriter("C:/Users/Dell/Documents/CIE 123/Project/data.txt", true);
+		/*FileWriter write = new FileWriter("C:/Users/Dell/Documents/CIE 123/Project/data.txt", true);
 		PrintWriter print_line= new PrintWriter(write);
 		print_line.printf("%s" + "%n", "Hello");
+		 
+		 print_line.close();
+		assertTrue(writeToFile("Hello"));*/
+		
+		FileWriter write = new FileWriter("D:/My Files/5th year 2nd sem/CIE123/project/data.txt", true);
+		PrintWriter print_line= new PrintWriter(write);
+		DateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date logdate = new Date();
+		String dateoflog;
+		dateoflog = date.format(logdate);
+		for (int i=0; i<1000; i++) //this will write 100 lines to the text file
+		{
+		print_line.printf("%s" + "%n",  " This is a stress test line. iteration " + i + "  " + dateoflog);
+		}
 		 
 		 print_line.close();
 		assertTrue(writeToFile("Hello"));
