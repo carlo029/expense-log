7587595d441929455a89c7fcfc71f47d42d5307a
carlo029 <antioquia_carlo2006@yahoo.com>
2015-02-26 16:09:31 +0800

    Added functions for filtering display of weekly, daily, monthly, and all expenses. Also added function for displaying the total expense

--- src/textfiles/Main.java
+++ src/textfiles/Main.java
@@ -24,12 +24,18 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException {
+		DateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date logdate = new Date();
+		String dateoflog;
+		dateoflog = date.format(logdate);
 		int flag = 1;
 		while (flag==1)
 		{
 		// TODO Auto-generated method stub
 		String choice;
+		String duration;
 		Scanner choicein = new Scanner (System.in);
+		Scanner durationin = new Scanner (System.in);
 		System.out.println("What do you want to do? (enter 1 or 2. Enter exit to exit the program.)");
 		System.out.println("1.Read expense log");
 		System.out.println("2. Input expense");
@@ -41,17 +47,127 @@ public class Main {
 		Scanner userinput = new Scanner (System.in);
 		System.out.print("Please enter file to open: ");
 		file_name =  userinput.nextLine();
-		
+		System.out.println("Please choose range of expense log data to display (enter 1, 2, 3, or 4)");
+		System.out.println("1. Month");
+		System.out.println("2. Week");
+		System.out.println("3. Day");
+		System.out.println("4. All");
+		duration = durationin.nextLine();
 		try {
 			ReadFile file =  new ReadFile(file_name);
 			String[] aryLines = file.OpenFile();
+			String numberholder="";
+			String temp="";
+			int sumofexpense=0;
 			
 			int i;
 			for ( i=0; i < aryLines.length; i++ ) {
+			
 			System.out.println( aryLines[ i ] ) ;
+			
+			temp=aryLines[i];
+			//start code for monthly sum
+			if (duration.equals("1"))	
+			{
+			//start of loop for sum
+			for(int x=0; x<temp.length(); x++)
+			{
+			if (temp.charAt(x)=='\t')//insert string comparison function here. If the character in the string is equal to a tab, then for arylines i to 0, store to an int value
+			{
+				if (temp.contains(dateoflog.substring(0,7)))
+				{	
+				for (int j=0; j<x; j++)
+					{//insert function for storing the int into a variable
+					numberholder += temp.charAt(j);
+					
+					}
+				sumofexpense += Integer.parseInt(numberholder);
+				numberholder="";
+				}
+				else {}
+				}
+			}
+			//end of loop for sum
+			}// end of code for monthly sum
+			
+			//start code for weekly sum
+			if (duration.equals("2"))	
+			{
+			//start of loop for sum
+				for(int x=0; x<temp.length(); x++)
+				{
+				if (temp.charAt(x)=='\t')//insert string comparison function here. If the character in the string is equal to a tab, then for arylines i to 0, store to an int value
+				{
+					if (Integer.parseInt(temp.substring(x+7,x+10))>=Integer.parseInt(dateoflog.substring(7,10))-3 && Integer.parseInt(temp.substring(x+7,x+10))<=Integer.parseInt(dateoflog.substring(7,10))+3 )
+					{	
+					for (int j=0; j<x; j++)
+						{//insert function for storing the int into a variable
+						numberholder += temp.charAt(j);
+						
+						}
+					sumofexpense += Integer.parseInt(numberholder);
+					numberholder="";
+					}
+					else {}
+					}
+				}
+			//end of loop for sum
+			}// end of code for weekly sum
+			
+			//start code for daily sum
+			if (duration.equals("3"))	
+			{
+			//start of loop for sum
+				for(int x=0; x<temp.length(); x++)
+				{
+				if (temp.charAt(x)=='\t')//insert string comparison function here. If the character in the string is equal to a tab, then for arylines i to 0, store to an int value
+				{
+					if (temp.contains(dateoflog.substring(0,10)))
+					{	
+					for (int j=0; j<x; j++)
+						{//insert function for storing the int into a variable
+						numberholder += temp.charAt(j);
+						
+						}
+					sumofexpense += Integer.parseInt(numberholder);
+					numberholder="";
+					}
+					else {}
+					}
+				}
+			//end of loop for sum
+			}// end of code for daily sum
+			
+			//start code for all sum
+			if (duration.equals("4"))	
+			{
+			//start of loop for sum
+				for(int x=0; x<temp.length(); x++)
+				{
+				if (temp.charAt(x)=='\t')//insert string comparison function here. If the character in the string is equal to a tab, then for arylines i to 0, store to an int value
+				{
+						
+					for (int j=0; j<x; j++)
+						{//insert function for storing the int into a variable
+						numberholder += temp.charAt(j);
+						
+						}
+					sumofexpense += Integer.parseInt(numberholder);
+					numberholder="";
+					
+					
+					}
+				}
+			//end of loop for sum
+			}// end of code for all sum
 			}
 			
-		}
+			
+			System.out.println("Total amount spent: " + sumofexpense);
+			
+			}
+			
+		
 		
 		catch (IOException e) {
 			System.out.println(e.getMessage());
@@ -69,10 +185,7 @@ public class Main {
 		String logvalue;
 		int number = Integer.parseInt(userinput.nextLine().replaceAll("[^\\d]", ""));
 		logvalue=String.valueOf(number);
-		DateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date logdate = new Date();
-		String dateoflog;
-		dateoflog = date.format(logdate);
+		//this is where the date code was
 		data.writeToFile(logvalue+"\t"+dateoflog);
 		
 		System.out.println("The string: " + logvalue + " has been written on the file.");}
